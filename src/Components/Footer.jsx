b9bbea84f7a2d9d83d63e49af51119917328c54e
Akshat Mittal <2004mittalakshat@gmail.com>
2025-05-28 09:56:01 +0530

    Changes

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -11,13 +11,13 @@ const Footer = () => {
   return (
     <div className='w-full h-full bg-green-700 text-white p-5'>
       <div className="container mx-auto flex flex-col items-center justify-around w-full h-full gap-5">
-        <div className="flex flex-row items-center justify-around w-full h-full">
+        <div className="flex md:flex-row flex-col items-center justify-around w-full h-full">
 
         <div className="brand flex flex-row items-center justify-center font-semibold">
           <img className='h-16' src="/brand-logo.png" alt="" />
           <h1>Aam'e Malihabad</h1>
         </div>
-        <ul className=" flex flex-row gap-10">
+        <ul className=" flex flex-row gap-10 text-xs">
           <h1>About Us</h1>
           <h1>Product</h1>
           <h1>Gallery</h1>
@@ -25,45 +25,45 @@ const Footer = () => {
         </ul>
         </div>
       <div className="middle w-full h-full flex flex-col items-center justify-center">
-        <div className="info flex flex-row items-start justify-start gap-5">
+        <div className="info flex md:flex-row flex-col items-start justify-start gap-5">
           <div className="location flex flex-row items-center justify-start gap-4 p-2 w-62 rounded-2xl">
-            <FaLocationDot className='text-red-800 text-3xl bg-white p-1 rounded-md' />
+            <FaLocationDot className='text-red-800 md:text-3xl text-xl bg-white p-1 rounded-md' />
             <div className="right flex flex-col items-start justify-start gap-1 ">
-              <p>Location,Location,India</p>
+              <p className='text-sm'>Location,Location,India</p>
             </div>
           </div>
           <div className="contact flex flex-row items-center justify-start gap-4 p-2 w-62 rounded-2xl">
-            <FaPhoneAlt className='text-3xl text-blue-900 bg-white p-1 rounded-md' />
+            <FaPhoneAlt className='md:text-3xl text-xl text-blue-900 bg-white p-1 rounded-md' />
             <div className="right flex flex-col items-start justify-start gap-1 ">
-              <p>91+ 1234567890</p>
+              <p className='text-sm'>91+ 1234567890</p>
             </div>
           </div>
           <div className="timming flex flex-row items-center justify-start gap-4 p-2 w-62 rounded-2xl">
-            <IoMdTime className='text-3xl text-blue-700 bg-white p-1 rounded-md' />
+            <IoMdTime className='md:text-3xl text-xl text-blue-700 bg-white p-1 rounded-md' />
             <div className="right flex flex-col items-start justify-start gap-1 ">
-              <h1 className='font-semibold'>Opening - Closing</h1>
-              <p>8am - 10pm</p>
+              <h1 className='font-semibold text-sm '>Opening - Closing</h1>
+              <p className='text-sm'>8am - 10pm</p>
             </div>
           </div>
         </div>
-        <div className="cards items-start justify-start flex flex-row gap-5">
+        <div className="cards items-start justify-start flex md:flex-row flex-col gap-5">
           <div className="whatsapp flex flex-row items-center justifystart gap-4 p-2 w-62 rounded-2xl">
-            <FaWhatsapp className='text-3xl text-green-600 bg-white p-1 rounded-md' />
+            <FaWhatsapp className='md:text-3xl text-xl text-green-600 bg-white p-1 rounded-md' />
             <div className="right flex flex-col items-start justify-start gap-1 ">
-              <h1 className='font-semibold'>Whatsapp</h1>
+              <h1 className='font-semibold text-sm'>Whatsapp</h1>
             </div>
           </div>
           <div className="email flex flex-row items-center justify-start gap-2 p-2 w-62 rounded-2xl">
-            <MdEmail className='text-3xl text-yellow-600 bg-white p-1 rounded-md' />
+            <MdEmail className='md:text-3xl text-xl text-yellow-600 bg-white p-1 rounded-md' />
             <div className="right flex flex-col items-start justify-start gap-1 md:text-md ">
-              <h1 className='font-semibold'>Email</h1>
+              <h1 className='font-semibold text-sm'>Email</h1>
             </div>
 
           </div>
           <div className="insta flex flex-row items-center justify-start gap-2 p-2  w-62 rounded-2xl ">
-            <FaInstagram className='text-3xl text-pink-800 bg-white p-1 rounded-md' />
+            <FaInstagram className='md:text-3xl text-xl text-pink-800 bg-white p-1 rounded-md' />
             <div className="right flex flex-col items-start justify-start gap-1">
-              <h1 className='font-semibold'>Instagram</h1>
+              <h1 className='font-semibold text-sm'>Instagram</h1>
             </div>
           </div>
 
@@ -71,7 +71,7 @@ const Footer = () => {
       </div>
       <div className="bottom font-medium flex flex-row items-center justify-center gap-2">
         <FaRegCopyright />
-        <h1>2025 Aam'e Malihabad</h1>
+        <h1 className='text-sm'>2025 Aam'e Malihabad</h1>
       </div>
 
     </div>
